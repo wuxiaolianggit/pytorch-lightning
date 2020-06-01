a19c6d80556c43c96b2013a7abf85fdbe2e6f15f
Justus Schock <justus.schock@rwth-aachen.de>
2020-06-01 14:14:08 +0200

    fix imports

--- pytorch_lightning/trainer/supporters.py
+++ pytorch_lightning/trainer/supporters.py
@@ -1,6 +1,7 @@
 import torch
 from pytorch_lightning.utilities.apply_func import apply_to_collection
 from collections.abc import Iterable, Iterator, Mapping, Sequence
+from typing import Any, Union
 
 
 class TensorRunningAccum(object):
